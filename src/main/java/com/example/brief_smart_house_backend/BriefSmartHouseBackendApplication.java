b07c3160504f1f66839b56765ccb552a998eb0f2
loubnas <loubnasoussi15489@gmail.com>
2022-04-11 20:36:51 +0000

    first push

---
+++ src/main/java/com/example/brief_smart_house_backend/BriefSmartHouseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.brief_smart_house_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriefSmartHouseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BriefSmartHouseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/brief_smart_house_backend/BriefSmartHouseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brief_smart_house_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriefSmartHouseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
